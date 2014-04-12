0a847be5aeffa1d343ca3d887b998a08ef2a496e
ckcollab <eric@ckcollab.com>
2014-04-12 00:12:32 -0700

    adding base theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,13 +16,19 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-#LINKS =  (,)
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+GITHUB_URL = 'http://github.com/ckcollab/'
+THEME = "theme"
