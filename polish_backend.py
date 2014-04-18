2bfa1b1264391701d2830cedd1c82a2cfa1e879f
Eric Carmichael <eric@ckcollab.com>
2014-04-18 11:08:57 -0700

    saving spot

--- polish_backend.py
+++ polish_backend.py
@@ -1,5 +1,19 @@
 from polished.backends import PelicanBackend
+from polished.decorators import polish
 
 
 class EricPelicanBackend(PelicanBackend):
-    pass
+
+
+
+
+
+    # you can call execute javascript to reset A tag urls and shit!!! so smart
+
+    @polish(urls=["tree trunk.html"])
+    def test_func(self):
+        print 'riddeeee'
+
+    @polish(commit_indexes=range(1, 5))
+    def test_funcer(self):
+        print 'riddeeee'
