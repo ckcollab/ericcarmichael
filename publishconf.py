f2a25bf067e31922c39a36f85b50aacd8a14f4c3
ckcollab <eric@ckcollab.com>
2014-04-18 13:59:00 -0700

    trying to fix sitemap

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-#SITEURL = 'http://ericcarmichael.com'
+SITEURL = 'http://ericcarmichael.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
